7de14dff376a1d9b2008531f38c62c49a057e49f
torresurban <99027914+torresurban@users.noreply.github.com>
2022-03-09 16:55:38 -0500

    Update tasks.routes.js

--- src/routes/tasks.routes.js
+++ src/routes/tasks.routes.js
@@ -21,13 +21,9 @@ const router = Router();
 
 //creando una simple url
 router.get("/tasks", getAllTasks);
-
 router.get("/tasks/:id", getTask);
-
 router.post("/tasks", createTask);
-
 router.delete("/tasks/:id", deleteTask);
-
 router.put("/tasks/:id", updateTask);
 
 module.exports = router;
